87aa0ead95039cf7dcb6f97f3a414078529b226b
zhafei <buer_y@163.com>
2021-04-23 23:25:41 +0800

    调整

--- tm-ums/tm-ums-admin/src/main/java/com/zhafei/ums/dao/UmsSysUserDao.java
+++ tm-ums/tm-ums-admin/src/main/java/com/zhafei/ums/dao/UmsSysUserDao.java
@@ -1,7 +1,9 @@
 package com.zhafei.ums.dao;
 
 import com.zhafei.ums.domain.entity.UmsSysUser;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UmsSysUserDao {
     int deleteByPrimaryKey(Long id);
 
