e761e461a2704892846eade5dde1f36bacaf9f6d
zhafei <buer_y@163.com>
2021-04-25 22:38:16 +0800

    nacos

--- tm-wechat/src/main/java/com/zhafei/wechat/TmWeChatApplication.java
+++ tm-wechat/src/main/java/com/zhafei/wechat/TmWeChatApplication.java
@@ -2,6 +2,7 @@ package com.zhafei.wechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @EnableFeignClients 表示启用feign
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 public class TmWeChatApplication {
     public static void main(String[] args) {
