ea64e5154a799b9c124c390a1d3ba39e7e97be3c
zhafei <buer_y@163.com>
2021-04-25 22:38:34 +0800

    openfeign

--- tm-wechat/src/main/java/com/zhafei/wechat/feignservice/TmPayFeignService.java
+++ tm-wechat/src/main/java/com/zhafei/wechat/feignservice/TmPayFeignService.java
@@ -2,13 +2,16 @@ package com.zhafei.wechat.feignservice;
 
 import com.zhafei.wechat.common.vo.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.math.BigDecimal;
 
+/**
+ * 服务来源与nacos注册中心
+ */
 @FeignClient(name="tm-pay-service",value="tm-pay-service")
 public interface TmPayFeignService {
-
-    @PostMapping("wechatpay")
-    public abstract Result<Object> weChatPay(BigDecimal value);
+    @PostMapping(value="wechatpay/{value}")
+    public abstract Result<Object> weChatPay(@PathVariable BigDecimal value);
 }
