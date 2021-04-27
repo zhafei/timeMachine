8ad4481a0919f55561ea42152acdb0b3c30e533c
zhafei <buer_y@163.com>
2021-04-27 17:43:26 +0800

    使用seata管理分布式事务

--- tm-wechat/src/main/java/com/zhafei/wechat/rest/RedPacketController.java
+++ tm-wechat/src/main/java/com/zhafei/wechat/rest/RedPacketController.java
@@ -2,6 +2,7 @@ package com.zhafei.wechat.rest;
 
 import com.zhafei.wechat.common.vo.Result;
 import com.zhafei.wechat.feignservice.TmPayFeignService;
+import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,15 @@ import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/redpacket")
+
 public class RedPacketController {
     @Autowired
     private TmPayFeignService tmPayFeignService;
     @PostMapping("give/{value}")
+    @GlobalTransactional
     public Result<Object> give(@PathVariable BigDecimal value){
         Result result =  tmPayFeignService.weChatPay(value);
+        int r = 10/0;
         return result;
     }
 }
